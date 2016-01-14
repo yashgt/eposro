1118c8d03f4e2ffb41eea3054ae167926231d07d
Nihal Prabhu Gaunkar <nihalprabhugaunkar@gmail.com>
2016-01-14 23:30:45 +0530

    Working Data Loader

--- epdb.js
+++ epdb.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient; // Driver for connecting to MongoDB
 var ObjectID =require('mongodb').ObjectID;
 var dbConn;
+
 MongoClient.connect('mongodb://localhost:27017/eposro', function(err, db) {
 	if(!err){
 		dbConn = db;
@@ -19,13 +20,13 @@ exports.saveProduct = function(product, cb){
 	{
         var query ={};
         var sort =[];
-        var operator ={$inc:{"last_entry_id":1}};
+        var operator ={$inc:{"last_pdt_id":1}};
         var options ={new:true};
         idcollection.findAndModify(query,sort,operator,options,
         			function(err,doc){
         				if(!err)
         				{
-        					id=doc.last_entry_id;
+        					id=doc.last_pdt_id;
 							product._id=id;
 							insert_product_internal(pcollection,product,cb);
         					
@@ -45,8 +46,112 @@ exports.saveProduct = function(product, cb){
 function insert_product_internal(collection,product,cb){
 	collection.insert(product,function(err,result){
 		if(!err){				
-			cb(result);
+			cb(null,result);
 			}
 		else{console.log(err);}
 		});
 }
+
+
+exports.findCategoryId =function(cat,cb){
+	var category = dbConn.collection('category');
+	category.find({"name":cat}).toArray(function(err,result){
+		if(!err){	
+			var cids=[];
+			for(var i=0;i<result.length;i++){
+				cids.push(result[i]._id);
+			}
+			cb(null,cids);
+		}
+		else{
+			console.log(err);
+		}
+	});
+};
+
+exports.findCityId = function(city,cb){
+	var cities= dbConn.collection('cities');
+	cities.findOne({"city":city.toLowerCase()},function(err,result){
+		if(!err){
+			cb(null,result._id);
+		}
+		else{
+			console.log(err);
+		}
+	});
+};
+
+exports.getLastPdtId= function(cb){
+	var IDs = dbConn.collection('IDs');
+	IDs.findOne(function(err,res){
+		if(!err){
+			cb(null,res.last_pdt_id);
+		}else{console.log(err);}
+	});
+};
+
+exports.getProductVars = function(pname,brand,cb){
+	var pcollection = dbConn.collection('products');
+	pcollection.find({"pname":pname,"brand":brand}).toArray(function(err,result){
+		if(!err){
+			//build a pseudo product
+			var vars=[];
+			for(var i=0;i<result.length;i++){
+				var product ={};
+				product.vid = result[i]._id;
+				product.vname = result[i].pname;
+				product.facets =result[i].facets;
+				vars.push(product);
+			}
+			cb(null,vars);
+		}
+		else{console.log(err);}
+	});
+};
+
+exports.getRelatedPdts = function(last_pdt_id,cb){
+	var pcollection = dbConn.collection('products');
+	//generate random 2 products
+	var rand = Math.floor(Math.random()*last_pdt_id),srand;
+	var relPdts=[];
+	pcollection.findOne({_id:rand},function(err,result){
+		if(!err){
+			if(result!=null){
+			var pdt={};
+			pdt.id=result._id;
+			pdt.pname=result.pname;
+			pdt.img=result.img;
+			relPdts.push(pdt);
+			}
+			srand = Math.floor(Math.random()*last_pdt_id);
+			pcollection.findOne({_id:srand},function(err,result){
+			if(!err){
+				if(result!=null){
+					pdt={};
+					pdt.id=result._id;
+					pdt.pname=result.pname;
+					pdt.img=result.img;
+					relPdts.push(pdt);
+					}
+				cb(null,relPdts);
+				}
+				else{console.log(err);}
+				});
+		}else{console.log(err);}
+	});
+};
+
+exports.writeLastPdtId = function(last_id,cb){
+	var IDs = dbConn.collection('IDs');
+	IDs.findOne(function(err,res){
+		if(!err){
+			IDs.update({_id:res._id},{ $set: { "last_pdt_id":last_id}},function(err,res){
+				if(!err){
+					cb(null,res);
+				}
+				else{console.log(err);}
+			});
+		}
+		else{console.log(err);}
+	});
+};
--- populate.js
+++ populate.js
@@ -1,102 +1,93 @@
-var epdb=require('./epdb');
+var epdb= require('./epdb');//file conatains all the database operations
 var XLSX = require("xlsx");
+var wait= require('wait.for');
 
-/////////////////////////////
-//functions needs to be completed
-var categories = ["Cow milk","Goat Milk","Buffalo Milk","Cone","Flavoured Milk"];
-function findCategory(cat){
-    var x=[];
-    for(var i=0;i<categories.length;i++){
-        if(categories[i]==cat){
-            x.push(i);
-        }
-    }
-    if(x.length!=0){
-        return x;
-    }
-    return -1;
-}
-function findCityID(city){
-    return 0;
-}
+setTimeout(function(){
+	var excel_file = process.argv[2];//read the excel sheet from the commandline args
+	var workbook =XLSX.readFile(excel_file);
+	var worksheet,current_sheet, data;
 
-////////////////////////////////
+	wait.launchFiber(function(){//running the async code in sync
+		for(var i = 1;i<workbook.SheetNames.length;i++){//read each worsheet from workbook
+			current_sheet = workbook.SheetNames[i];//current sheet name
+    		worksheet=workbook.Sheets[current_sheet];
 
-var excel_file = process.argv[2];
-var workbook =XLSX.readFile(excel_file);
-var worksheet,current_sheet, data;
+    		data=XLSX.utils.sheet_to_json(worksheet);//read entire data of the worksheet in json format
 
-for(var i = 1;i<workbook.SheetNames.length;i++){
-    current_sheet = workbook.SheetNames[i];
-    worksheet=workbook.Sheets[current_sheet];
+    		if(JSON.stringify(data)!='[]'){//if worsheet is not empty then get single product from each row
+    			var keys = Object.keys(data[0]);//get all the column names of the worksheet
+    		
+    			//read all the facets like fat%, energy etc
+    			var facets=[],j;
+        		var facet_index=7;//assuming that facets starts from certain index
+       	 		for(j=facet_index;keys[j].search("MRP")==-1;j++){//facets will be there till MRP of cities is not mentioned
+            		facets.push(keys[j]);
+        		}
+        	
+        		//find all the cities in which the product is sold
+       			var mrp_index=j;//index at which price in each city starts
+       			var cities = [];
+        		for(var k=mrp_index;k<keys.length;k++){
+            		cities.push(keys[k].substring(3,keys[k].length));//first three places contain MRP so neglect it.
+        		}
+        	
+        		var cats=wait.for(epdb.findCategoryId,current_sheet);//categories to which product belongs
+        		var last_pdt_id=wait.for(epdb.getLastPdtId);//last entered id in database
 
-    data=XLSX.utils.sheet_to_json(worksheet);
+        		for(j=0;j<data.length;j++){
+        			if(data[j]['_id']=='-'){
+        				var product={};//build a single product
+        				product['_id']=++last_pdt_id;
+        				product.brand=data[j]['Brand'];
+        				product.gtin = data[j]['GTIN'];
+        				product.pname = data[j]['Title'];
+        				product.desc = data[j]['Description'];
+        				product.img = data[j]['Image_URL'];
+        				product.cats = cats;
+        			
+        				product.facets={};
+        				for(var k =facet_index;k<mrp_index;k++){
+            				var temp=keys[k].replace(/" "/g,"_").toLowerCase();
+            				if(data[j][keys[k]].match(/^[0-9]+.?[0-9]*$/)!=null){//it contains only number
+                				if(data[j][keys[k]].match(/^[0-9]+$/)!=null)
+                    				product.facets[temp]=parseInt(data[j][keys[k]].match(/^[0-9]+$/).toString());
+                				else
+                    				product.facets[temp]=parseFloat(data[j][keys[k]].match(/^[0-9]+.[0-9]+$/).toString());
+            				}//end of if statement
+            				else{
+                				product.facets[temp]={};
+                 				if(data[j][keys[k]].search(/\./)==-1)
+                    				product.facets[temp]['val']=parseInt(data[j][keys[k]].match(/^[0-9]+/).toString());
+                				else
+                     				product.facets[temp]['val']=parseFloat(data[j][keys[k]].match(/^[0-9]+.[0-9]+/).toString());
+   
+                				product.facets[temp]['unit'] =data[j][keys[k]].match(/[a-zA-Z]+$/).toString();
+            				}//end of else part
+        				}//end of for loop of facets
 
-    if(JSON.stringify(data)!='[]'){
-        var keys = Object.keys(data[0]);
-
-        //find all the facets for the products
-        var facets=[];
-        var facet_index;
-        for(facet_index=7;keys[facet_index].search("MRP")==-1;facet_index++){
-            facets.push(keys[facet_index]);
-        }
-        var mrp_index=facet_index;
-        facet_index=7;
-
-        //find all the cities which our product is present
-        var cities = [];
-        for(var k=mrp_index;k<keys.length;k++){
-            cities.push(keys[k].substring(3,keys[k].length));
-        }
-
-        //read last product id from the database
-        //var last_id = getNextId();
-        var last_id =0;
-        //find categories to which the products from this category might belong
-        var cats = findCategory(current_sheet);
-        //develop the product 
-      for(var j=0;j<data.length;j++){
-        var product={};
-        product['_id']=last_id++;
-        product.brand=data[j]['Brand'];
-        product.gtin = data[j]['GTIN'];
-        product.pname = data[j]['Title'];
-        product.desc = data[j]['Description'];
-        product.img = data[j]['Image_URL'];
-        product.cats = cats;
-        product.facets={};
-        for(var k =facet_index;k<mrp_index;k++){
-            var temp=keys[k].replace(/ /g,"_").toLowerCase();
-            if(data[j][keys[k]].match(/^[0-9]+.?[0-9]*$/)!=null){//it contains only number
-                if(data[j][keys[k]].match(/^[0-9]+$/)!=null)
-                    product.facets[temp]=parseInt(data[j][keys[k]].match(/^[0-9]+$/).toString());
-                else
-                    product.facets[temp]=parseFloat(data[j][keys[k]].match(/^[0-9]+.[0-9]+$/).toString());
-            }
-            else{
-                product.facets[temp]={};
-                 if(data[j][keys[k]].search(/\./)==-1)
-                    product.facets[temp]['val']=parseInt(data[j][keys[k]].match(/^[0-9]+/).toString());
-                else
-                     product.facets[temp]['val']=parseFloat(data[j][keys[k]].match(/^[0-9]+.[0-9]+/).toString());
-                
-                product.facets[temp]['unit'] =data[j][keys[k]].match(/[a-zA-Z]+$/).toString();
-            }
-        }
-        product.price={};
-        product.price.default_mrp=data[j]['MRP'];
-        product.price.mrp=[];
-        temp=0;
-        for(var k= mrp_index;k<keys.length;k++){
-            product.price.mrp[temp]={};
-            product.price.mrp[temp].city=findCityID(cities[temp]);
-            product.price.mrp[temp].mrp=data[j]["MRP"+cities[temp]];
-            temp++;
-        }
-        product.vars=[];
-        product.rel_pdts=[];
-      }
-    }
-}
-
+        				product.price={};
+        				product.price.default_mrp=data[j]['MRP'];
+       					product.price.mrp=[];
+        				temp=0;
+        				for(var k= mrp_index;k<keys.length;k++){
+                    		product.price.mrp[temp]={};
+                    		product.price.mrp[temp].city=wait.for(epdb.findCityId,cities[temp]);
+                    		product.price.mrp[temp].mrp=data[j]["MRP"+cities[temp]];
+                    		temp++;
+                		}//end of for loop for city mrp
+                		product.vars=wait.for(epdb.getProductVars,product.pname,product.brand);
+                		product.rel_pdts= wait.for(epdb.getRelatedPdts,last_pdt_id);//currently gets random products from DB
+             
+                		var result=wait.for(epdb.saveProduct,product);
+                		//update the workbook with the added product
+                		var address_of_cell ="A"+(j+2).toString();
+                		workbook.Sheets[current_sheet][address_of_cell].v=(last_pdt_id-1)
+                		console.log("Inserted product");
+                	}//end of if statement for checking id
+        		}//end of forloop for each product
+        		var result=wait.for(epdb.writeLastPdtId,last_pdt_id);
+    		}//end of if statement
+		}//end of main for loop
+	XLSX.writeFile(workbook, excel_file);//finally writing the workbook to excel
+	});//end of wait-for loop 
+},10000);//end of setTimeout function
