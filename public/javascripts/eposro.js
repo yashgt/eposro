0ece277bb8069f61eaaa6bf3069823ffffb9437c
Sohan Shirodkar <shirodkarsohan@gmail.com>
2016-02-06 12:22:52 +0530

    Product directive merged with index.js

--- public/javascripts/eposro.js
+++ public/javascripts/eposro.js
@@ -2,73 +2,106 @@ function EposroController(
 	$scope
 	,$log
 	,$http
-) {
-	//$scope.mycart = { count: 20 };
- 
-    $scope.products = [];
-	$scope.pdtsPage = 0;
-	$scope.busy = false;
+    ,$mdSidenav
+){         
+        $scope.products = [];
+        $scope.pdtsPage = 1;
+        $scope.busy = false;
+        $scope.breadCrumbs = [];
         
-	$http.get("/api/categories").success( 
-		function(response){
-			$scope.cats = response;
-            console.log(response);
-           
-        }
-    );
     
-    $scope.loadMore = function() {
-        if ($scope.busy) return;
-            $scope.busy = true;
-		$http.get('/api/products/'+$scope.pdtsPage).success(
-			function(response){
-				$scope.pdtsPage++;
+        $http.get("/api/categories").success( 
+            function(response){
+                $scope.categories = response;
+            }
+        );
+        
+        $scope.fetchCategoryProducts = function(cat_id){
+            //$scope.products = [];
+            $scope.pdtsPage = 1;
+            $http.get("/api/products/"+cat_id).success(
+                function(response){
+                    console.log("Made call for page  1 of category "+cat_id);
+                    for( var i = 0; i<response.length; i++){
+                        $scope.products.push(response[i]);
+                    }
+                }
+            )
+        };
+        
+        $scope.clearProducts = function(){
+            $scope.products = [];
+            $scope.pdtsPage = 0;
+        }
+            
+      $scope.loadMore = function(cat_id) {
+            //console.log("called loadMore()");
+            if ($scope.busy) return;
+                $scope.busy = true;
+            $http.get('/api/products/'+cat_id+'/'+$scope.pdtsPage).success(
+                function(response){
+                    $scope.pdtsPage++;
+                    console.log("Made call for page  "+$scope.pdtsPage+" of category "+cat_id);
+                    for (var i = 0; i < response.length; i++) {
+                        $scope.products.push(response[i]);
+                    }
+                    $scope.busy = false;
+                });
+        }
+        
+        $scope.openRightMenu = function() {
+            $mdSidenav('right').toggle();
+        }
+        
+        $scope.setBreadArray = function(cat,i){
+            
+            if( i == 0){
+                // this is called by a category
+                $scope.breadCrumbs = [];
                 
-				for (var i = 0; i < response.length; i++) {
-					$scope.products.push(response[i]);
-				}
-                $scope.busy = false;
-			});
-	}    
+            }
+            else{
+                if( $scope.breadCrumbs.length > 1){
+                    $scope.breadCrumbs.pop();
+                }
+            }
+            $scope.breadCrumbs.push(cat);
+        }       
 }
 	
-function CatsController(
-    $scope
-    ,$log
-    ,$http
-){
-    $scope.categories = [
-        {title:"Dairy",content:"Fetch all dairy products from the database"},
-        {title:"Staple",content:"Fetch all staple diet products from the database"},
-        {title:"Bakery",content:"Fetch all bakery products from the database"},
-        {title:"Beverages",content:"Fetch all beverages from the database"},
-        {title:"Hygiene",content:"Fetch all hygiene products from the database"},
-        {title:"Baby Care",content:"Fetch all baby care products from the database"},
-        {title:"Eatables",content:"Fetch all eatables from the database"},
-        {title:"Home Utility",content:"Fetch all home utility products from the database"},
-        {title:"Eggs",content:"Fetch all egg products from the database"}
-    ]
-    
-}	
-	
+
+
 (function() {
     var epapp = angular.module('epapp', [
 		'ngTouch'
 		,'infinite-scroll'
         ,'ngMaterial'
     ]);
-	epapp.controller('CatsController',[
-        '$scope'
-        ,'$log'
-        ,'$http'
-        ,CatsController]);
     
+    epapp.config(function($mdThemingProvider) {
+        
+        $mdThemingProvider.theme('default')
+        .primaryPalette('indigo')
+        .accentPalette('blue');
+    });
+
+    epapp.directive('product',function(){
+        return{
+            
+            restrict:'AE',
+            templateUrl:'productdirective.html',
+            replace: true,
+            scope: {
+                product : '=pro'
+            }
+        };
+    });
+
 	epapp.controller('EPController', [
 	'$scope'
-	, '$log'
-	, '$http'
+    ,'$log'
+    ,'$http'
+    ,'$mdSidenav'
 	,  EposroController]);
-    
-	
 })();
  
