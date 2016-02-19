bf433a72fa9f6fe3bff57e3cc758984a1f431951
unknown <yashgt@gmail.com>
2016-02-19 20:30:19 +0530

    solr fixed

--- solr.js
+++ solr.js
@@ -1,17 +1,35 @@
 var solr = require('solr-client');
 var client = solr.createClient("127.0.0.1","9393","eposro");
 
-exports.getProducts = function(pname,cb){
-    
-	var client = solr.createClient("127.0.0.1","9393","eposro");
-	var query = client.createQuery()
-				.q(pname)
-				.dismax();
-				
+doSearch = function(client, query, cb){
 	client.search(query,function(err,obj){
 		if(err) 
 			console.log(err);
-		else
+		else{
+			console.log(obj.response.docs);
 			cb(obj.response.docs);
+		}	
 	});
 };
+exports.getProductsBySearchString = function(pname,cb){
+    
+	var client = solr.createClient("127.0.0.1","9393","eposro");
+	var query = client.createQuery() //  /select
+				.q(pname)
+				.dismax();
+		
+	
+};
+
+exports.getProductsByCategory = function(catID,cb){
+    
+	var client = solr.createClient("127.0.0.1","9393","eposro");
+	var query = client.createQuery()
+				.q({cats: catID})
+				;
+	doSearch(client, query, cb);					
+	
+};
+
+//exports.getProductsBySearchString("Amul", function(docs){});
+exports.getProductsByCategory(6, function(docs){});
