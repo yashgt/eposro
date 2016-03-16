630c2f00fda63d8f56af8af20d3216b38329057c
unknown <yashgt@gmail.com>
2016-03-16 10:38:17 +0530

    changes

--- mean/config/env/development.js
+++ mean/config/env/development.js
@@ -4,13 +4,11 @@ var defaultEnvConfig = require('./default');
 
 module.exports = {
   db: {
-    uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean-dev',
+    uri: 'mongodb://localhost:40000/eposrodb',
     options: {
       user: '',
       pass: ''
-    },
-    // Enable mongoose debug mode
-    debug: process.env.MONGODB_DEBUG || false
+    }
   },
   log: {
     // logging with Morgan - https://github.com/expressjs/morgan
