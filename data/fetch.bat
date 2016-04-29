efbd2ce04faa179329176f9e30819016385b05dd
Yash Ganthe <yashgt@gmail.com>
2016-04-29 12:39:37 +0530

    fetch gs1

--- data/fetch.bat
+++ data/fetch.bat
@@ -1,2 +1,4 @@
 tidy -xml -asxml -o a.xml gs1datakart.products.pilot.xml
-sed "s/\(https:\/\/.*.jpg\)/<a href=\"\1\">Link<\/a>/g" a.xml > b.xml
+sed "s/\(https:\/\/.*.jpg\)/\1/g" a.xml | grep https | sed "s/<\/.*//g" > b.xml
+
+wget64.exe --no-check-certificate -A jpg -P images -v -d -i b.xml
