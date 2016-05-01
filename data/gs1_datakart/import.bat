55c314f32ea89979d1a034a90501969e788e48a7
Nihal Prabhu Gaunkar <nihalprabhugaunkar@gmail.com>
2016-05-01 16:24:55 +0530

    changes

--- data/gs1_datakart/import.bat
+++ data/gs1_datakart/import.bat
@@ -1,3 +1,4 @@
 mongoimport -d eposro -c gs1cats --host localhost:40000 --jsonArray --upsert categories.gs1datakart.pilot.json
 mongoimport -d eposro -c gs1subcats --host localhost:40000 --jsonArray --upsert subcategories.gs1datakart.pilot.json
 mongoimport -d eposro -c gs1companies --host localhost:40000 --jsonArray --upsert companies.gs1datakart.pilot.json
+mongoimport -d eposro -c gs1data --host localhost:40000 --jsonArray --upsert gs1datakart.products.pilot.json
