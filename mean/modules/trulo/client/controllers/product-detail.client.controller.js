d120f403546c423d94ca1d65ddb7909941eef831
Sohan Shirodkar <shirodkarsohan@gmail.com>
2016-06-15 18:51:13 +0530

    Changes to product detail page

--- mean/modules/trulo/client/controllers/product-detail.client.controller.js
+++ mean/modules/trulo/client/controllers/product-detail.client.controller.js
@@ -3,11 +3,12 @@
 angular.module('trulo').controller('ProductDetailController', ['$scope', '$stateParams','Trulo','Mycart',
   function ($scope,$stateParams,trulo,myCart) {
       $scope._id = $stateParams._id;
-      console.log("Received details req for "+$stateParams._id);
+      //console.log("Received details req for "+$stateParams._id);
       //$scope.productCount = myCart.getCount($scope.product);;
       
       trulo.getProductById($scope._id, function(productResponse){
           $scope.product = productResponse;
+          //console.log("Showing "+$scope.product.img.front);
           console.log($scope.product);
           $scope.productCount = myCart.getCount($scope.product);
           $scope.quantity = $scope.productCount;
--- mean/modules/trulo/server/controllers/api.server.controller.js
+++ mean/modules/trulo/server/controllers/api.server.controller.js
@@ -48,7 +48,7 @@ exports.getRecommendations = function(req, res) {
     if (req.user != null) {
         var userID = parseInt(req.user._id);
         console.log('Inside Get Recommendations' + userID);
-        trulo.getClusterRecommendations(function(res) {
+        trulo.getClusterRecommendations(function(idResponse) {
             solr.getProductsById(idResponse,function(productsResponse){
                 res.send(productsResponse);
             });
